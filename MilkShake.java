3faa80c0a6353d480db3c1858c10c2ae616ac8cb
zakiasafara <49058072+zakiasafara@users.noreply.github.com>
2019-12-05 00:06:02 +0700

    memasukkan code

---
+++ MilkShake.java
@@ -0,0 +1,13 @@
+ 
+
+public class MilkShake extends Beverage {
+    @Override
+    public String getDescription() {
+        return "Milk Shake";
+    }
+
+    @Override
+    public double cost() {
+        return 1.25;
+    }
+}
